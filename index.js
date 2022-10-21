f63d7241b80a85586121a6e3c0aee8a3d22ef109
Eddie Espinosa <espinosa.edilson@gmail.com>
2022-10-21 14:48:16 -0400

    added function to find one article of clothing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { updateClothes } from "./src/clothes.js";
+import { updateClothes,findOneArticleOfClothing } from "./src/clothes.js";
 
 const PORT = 3030;
 const app = express();
@@ -8,5 +8,6 @@ app.use(cors());
 app.use(express.json());
 
 app.patch("/clothes/update/:id", updateClothes);
+app.get("/clothes/search/:search", findOneArticleOfClothing);
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}...`));
--- src/clothes.js
+++ src/clothes.js
@@ -1,19 +1,34 @@
 import dbConnect from "./dbConnect.js";
 
-export async function updateClothes(req,res){
+export async function updateClothes(req, res) {
     const db = dbConnect();
     const clothesCollection = db.collection("clothes");
 
-    const {field, value} = req.body;
-    const {id} = req.params;
+    const { field, value } = req.body;
+    const { id } = req.params;
     const update = {}
     update[field] = value;
 
     const result = await clothesCollection.doc(id).update(update)
-    .catch(err=>{
-        res.status(500).send({success:false, message: err});
-        return;
-    })
+        .catch(err => {
+            res.status(500).send({ success: false, message: err });
+            return;
+        })
 
     res.status(201).send(result);
+}
+
+export async function findOneArticleOfClothing(req, res) {
+    const db = dbConnect();
+    const clothesCollection = db.collection('clothes');
+
+    const { search } = req.params;
+
+    const result = await clothesCollection.doc(search).get()
+        .catch(err => {
+            res.status(500).send({ success: false, message: err });
+            return;
+        })
+    
+    res.status(201).send({ success: true, message: result.data() });
 }
